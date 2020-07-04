bb05c81d34ba7472ce8a7273403fe366e32aa4b8
Shiohne <Shiohne@github.com>
2020-07-04 17:03:43 -0500

    /places

--- src/app/services/favourite.service.ts
+++ src/app/services/favourite.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {Favourite} from "../model/favourite";
-import {Observable, of} from "rxjs";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
-import {map} from "rxjs/operators";
-import {Locatable} from "../model/locatable";
+import {Favourite} from '../model/favourite';
+import {Observable, of} from 'rxjs';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Locatable} from '../model/locatable';
 
 @Injectable({
   providedIn: 'root'
@@ -11,28 +11,28 @@ import {Locatable} from "../model/locatable";
 
 export class FavouriteService {
 
-  private urlEndPoint: string = 'https://goingto-open.azurewebsites.net/api/favourites';
-  favourite: Favourite=new Favourite();
+  private urlEndPoint = 'https://goingto-open.azurewebsites.net/api/favourites';
+  favourite: Favourite = new Favourite();
 
   constructor(private http: HttpClient) {
   }
 
-  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  getallfavourites():Observable<Favourite[]>{
-    return this.http.get<Favourite[]>(`${this.urlEndPoint}`)
+  getallfavourites(): Observable<Favourite[]>{
+    return this.http.get<Favourite[]>(`${this.urlEndPoint}`);
   }
 
 
   getFavouritesbyuserid(userid): Observable<Favourite[]> {
-    const url = 'https://goingto-open.azurewebsites.net/api/users'
-    return this.http.get<Favourite[]>(`${url}/` + userid + `/favourites`)
+    const url = 'https://goingto-open.azurewebsites.net/api/users';
+    return this.http.get<Favourite[]>(`${url}/` + userid + `/favourites`);
   }
 
   save(): Observable<Favourite> {
-    const url = 'https://goingto-open.azurewebsites.net/api/users'
+    const url = 'https://goingto-open.azurewebsites.net/api/users';
 
-    return this.http.post<Favourite>(`${url}/`+this.favourite.userId+`/locatables/`+this.favourite.locatableId, this.favourite,{headers:this.httpHeaders})
+    return this.http.post<Favourite>(`${url}/` + this.favourite.userId + `/locatables/` + this.favourite.locatableId, this.favourite, {headers: this.httpHeaders});
   }
 
   delete(locatableid): Observable<Favourite>
